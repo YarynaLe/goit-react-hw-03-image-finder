e42c1b5047b5e4f07d13f68c2b8ad26bb44b98ce
YarynaLe <109382584+YarynaLe@users.noreply.github.com>
2023-10-07 22:38:00 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,94 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { fetchQuery } from 'services/api';
+import { Searchbar } from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Button } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
+import { ModalWindow } from 'components/Modal/Modal';
+import { Container } from 'components/App.styled';
+import 'react-toastify/dist/ReactToastify.css';
+
+class App extends Component {
+  state = {
+    searchQuery: '',
+    images: [],
+    page: 1,
+    totalImages: 0,
+    loading: false,
+    largeImage: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery, page } = this.state;
+
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ loading: true });
+
+      fetchQuery(searchQuery, page)
+        .then(resp =>
+          this.setState(prevState => ({
+            images: [...prevState.images, ...resp.images],
+            totalImages: resp.TotalHits,
+          }))
+        )
+        .catch(error => console.log(error))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+
+  searchQueryValue = value => {
+    if (value === this.state.searchQuery) {
+      toast.warn('Enter another search query!');
+      return;
+    }
+
+    this.setState({
+      searchQuery: value,
+      page: 1,
+      totalImages: 0,
+      images: [],
+      showLoadMoreBtn: false,
+    });
+  };
+
+  getModalImage = imageUrl => {
+    this.setState({ largeImage: imageUrl });
+  };
+
+  largeImageStateReset = () => {
+    this.setState({ largeImage: '' });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  render() {
+    const { images, largeImage, loading, totalImages } = this.state;
+
+    return (
+      <Container>
+        <Searchbar onSubmit={this.searchQueryValue} />
+        <ImageGallery images={images} getModalImage={this.getModalImage} />
+        {loading && <Loader />}
+        {totalImages !== images.length && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+
+        <ToastContainer position="top-center" autoClose={1500} />
+
+        {largeImage && (
+          <ModalWindow
+            largeImage={largeImage}
+            largeImageStateReset={this.largeImageStateReset}
+          />
+        )}
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Modal } from 'react-responsive-modal';
+import 'components/Modal/styles.css';
+
+export const ModalWindow = ({ largeImage, largeImageStateReset }) => {
+  const [open, setOpen] = useState(true);
+
+  const onCloseModal = () => {
+    if (largeImage) {
+      largeImageStateReset();
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      {/* <button onClick={onOpenModal}>Open modal</button> */}
+      <Modal
+        open={open}
+        onClose={onCloseModal}
+        center
+        showCloseIcon={false}
+        animationDuration={200}
+      >
+        <img src={largeImage} alt="" />
+      </Modal>
+    </div>
+  );
+};
